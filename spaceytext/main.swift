04a527ea5dce5ca5cb2735ec71489018e4623a8b
toadhats <limule@icloud.com>
2016-01-10 15:58:10 +1100

    Initial Commit

---
+++ spaceytext/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  spaceytext
+//
+//  Created by Jonathan Warner on 10/01/2016.
+//  Copyright © 2016 Jonathan Warner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
